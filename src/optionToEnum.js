b222377d81bc527a3c71c4e2bf7a7dc2ab978c54
Jesus Almaraz martín <JalmarazMartn@gmail.com>
2023-09-02 18:06:57 +0200

    Get captions

--- src/optionToEnum.js
+++ src/optionToEnum.js
@@ -3,18 +3,25 @@ const regexpField = /field\((.*);(.*);\s*option/i;
 const vscode = require('vscode');
 const carriage = '\r\n';
 const sep = ';';
+const regexOptionMembers = /OptionMembers\s*=\s*(.+);/;
+const regexOptionCaptions = /OptionCaption\s*=\s*\'(.+)\';/;
 
 module.exports = {
     createOptionsCSV: function () {
         createOptionsCSV();
-    }
+    },
+	ProcessEnumFile: function()
+	{
+		ProcessEnumFile();
+	}
+
 }
 
 async function createOptionsCSV()
 {
 	const sep = ';';
 	const AllDocs = await vscode.workspace.findFiles('**/*.{al}');    
-	var FinalText = 'Object Type' + sep + 'Object Number'+ sep +'FieldId' + sep +'Field Name' + sep + 'Option Members' + sep + 'New Enum Id' + sep + 'New Enum Name' + carriage;
+	var FinalText = 'Object Type' + sep + 'Object Number'+ sep +'FieldId' + sep +'Field Name' + sep + 'Option Members' + sep + 'Option Captions' + sep +'New Enum Id' + sep + 'New Enum Name' + carriage;
 	for (let index = 0; index < AllDocs.length; index++) {
 		var ALDocument = await vscode.workspace.openTextDocument(AllDocs[index]);
         FinalText = FinalText + await GetFieldsTextFromTableExtension(ALDocument);
@@ -36,7 +43,8 @@ async function GetFieldsTextFromTableExtension(ALDocument) {
 	for (let index = 1; index < ALDocument.lineCount - 1; index++) {        
 		let matchField = ALDocument.lineAt(index).text.match(regexpField);
 		if (matchField) {			
-            fieldsText +=  objDeclaration[1] + sep + objDeclaration[2] + sep +matchField[1] + sep + matchField[2] + sep + getOptionMembers(ALDocument,index) + carriage;
+            fieldsText +=  objDeclaration[1] + sep + objDeclaration[2] + sep +matchField[1] + sep + matchField[2] + sep + getOptionValues(ALDocument,index,regexOptionMembers) + sep + 
+			getOptionValues(ALDocument,index,regexOptionCaptions) + carriage;
 		}
 	}
     return fieldsText;
@@ -52,13 +60,45 @@ function optionsCSVFile(newOpenLabel = '') {
 	};
 	return options;
 }
-function getOptionMembers(ALDocument,declineNumber=0)
+async function ProcessEnumFile() {
+	let enumsJSON = [];
+	let fileUri = await vscode.window.showOpenDialog(optionsCSVFile('Open'));
+	var fs = require('fs'),
+		readline = require('readline');
+
+	var rd = readline.createInterface({
+		input: fs.createReadStream(fileUri[0].fsPath)
+	});
+	rd.on('line', function (line) {
+		const Elements = line.split(sep);		
+		enumsJSON.push(
+			{
+				"ObjectType": Elements[0],
+				"ObjectNumber": Elements[1],
+				"FieldID": Elements[2],
+				"OptionMembers": Elements[4],
+				"OptionCaption": Elements[5],
+				"NewEnumID": Elements[6],
+				"NewEnumName": Elements[7]
+			});
+	});
+	rd.on('close',function () {			
+			//createEnumsAndReplace(enumsJSON);
+	}
+	);
+}
+
+function getOptionValues(ALDocument,declineNumber=0,regex)
 {
-    const regexOptionMembers = /OptionMembers\s*=\S*(.+);/;
+    
 	for (let index = declineNumber; index < ALDocument.lineCount - 1; index++) {        
-		let matchOption = ALDocument.lineAt(index).text.match(regexOptionMembers);
+		let matchOption = ALDocument.lineAt(index).text.match(regex);
 		if (matchOption) {			            
             return matchOption[1];
+		}
+		if (ALDocument.lineAt(index).text.indexOf("}") > -1)
+		{
+			return '';
 		}
 	}
     return '';
